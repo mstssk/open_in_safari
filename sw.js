39240648dad70e0c1665d1fc178a78cb64aeb7df
mstssk <mstssk+github@gmail.com>
2024-08-28 22:53:44 +0900

    Implements Chrome Extension

---
+++ sw.js
@@ -0,0 +1,18 @@
+// https://developer.chrome.com/docs/extensions/reference/api/contextMenus
+
+chrome.contextMenus.create({
+  id: "open_in_safari",
+  title: "Safariで開く",
+  type: "normal",
+  contexts: ["link"],
+  targetUrlPatterns: ["https://*/*", "http://*/*"],
+});
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === "open_in_safari") {
+    // x-safari-https://example.com/
+    const url = `x-safari-${info.linkUrl}`;
+    console.log(url);
+    chrome.tabs.update(null, { url });
+  }
+});
