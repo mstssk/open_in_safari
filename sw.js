61173414caec32dea2f8d762cbde784ee72eb6a2
mstssk <mstssk+github@gmail.com>
2025-09-09 23:49:25 +0900

    リンクだけでなく開いているページも対応

--- sw.js
+++ sw.js
@@ -1,18 +1,35 @@
 // https://developer.chrome.com/docs/extensions/reference/api/contextMenus
 chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create({
-    id: "open_in_safari",
-    title: "Safariで開く",
+    id: "open_in_safari-link",
+    title: "このリンクをSafariで開く",
     type: "normal",
     contexts: ["link"],
+    targetUrlPatterns: ["https://*/*", "http://*/*"],
+  });
+
+  chrome.contextMenus.create({
+    id: "open_in_safari-page",
+    title: "このページをSafariで開く",
+    type: "normal",
+    contexts: ["page"],
     targetUrlPatterns: ["https://*/*", "http://*/*"],
   });
 });
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
-  if (info.menuItemId === "open_in_safari") {
-    // x-safari-https://example.com/
-    const url = `x-safari-${info.linkUrl}`;
+  let url;
+  switch (info.menuItemId) {
+    case "open_in_safari-link":
+      // x-safari-https://example.com/
+      url = `x-safari-${info.linkUrl}`;
+      break;
+    case "open_in_safari-page":
+      // x-safari-https://example.com/
+      url = `x-safari-${info.pageUrl}`;
+      break;
+  }
+  if (url) {
     console.log(url);
     chrome.tabs.update(null, { url });
   }
